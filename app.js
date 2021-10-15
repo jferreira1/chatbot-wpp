16a25f521057bdf7dac548e7e7b4002dc7bf7d2a
Jean Ferreira <jeansouzza@gmail.com>
2021-10-15 16:17:50 -0300

    PORT listening changed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const qrcode = require("qrcode-terminal");
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
